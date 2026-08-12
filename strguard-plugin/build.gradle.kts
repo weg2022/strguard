365096529e4e32a7d95ecc9d12e45f96f81d746e
听雪客 <weg2022@outlook.com>
2026-08-12 23:12:34 +0800

    发布：升级 StrGuard 至 2.0.3

--- strguard-plugin/build.gradle.kts
+++ strguard-plugin/build.gradle.kts
@@ -17,7 +17,7 @@ jacoco {
 }
 
 group = "io.github.weg2022"
-version = providers.gradleProperty("strguardVersion").getOrElse("2.0.2")
+version = providers.gradleProperty("strguardVersion").getOrElse("2.0.3")
 
 val androidGradlePluginVersion = "9.3.1"
 val kotlinGradlePluginVersion = "2.4.10"
